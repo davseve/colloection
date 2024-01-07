c70d9f137e7df424f3a9e3f1df3ce1177f7eec04
David Seveloff <davids@elementor.com>
2024-01-07 10:14:19 +0200

    intenrnal: PHPdoc and indentation

--- src/Collection.php
+++ src/Collection.php
@@ -11,6 +11,16 @@ class Collection implements \Countable
 		$this->array = $array;
 	}
 
+    /**
+     * Get the first element of the collection.
+     *
+     * @example
+     * $collection = new Collection([1, 2, 3]);
+     * $result = $collection->first();
+     * // Returns: 1
+     *
+     * @return mixed
+     */
 	public function first() {
         if ( Collection_Helper::is_assoc( $this->array ) ) {
             $value = reset( $this->array );
@@ -24,6 +34,16 @@ class Collection implements \Countable
         }
 	}
 
+    /**
+     * Get the last element of the collection.
+     *
+     * @example
+     * $collection = new Collection([1, 2, 3]);
+     * $result = $collection->last();
+     * // Returns: 3
+     *
+     * @return mixed
+     */
 	public function last() {
         if ( Collection_Helper::is_assoc( $this->array ) ){
             $value = end( $this->array );
@@ -33,21 +53,63 @@ class Collection implements \Countable
         } else {
             return end( $this->array );
         }
-	}
+    }
 
-	public function merge( $addition ) {
+    /**
+     * Merge the collection with another array.
+     *
+     * @param array $addition
+     * @example
+     * $collection = new Collection([1, 2, 3]);
+     * $result = $collection->merge([4, 5, 6]);
+     * // Returns: Collection object with elements [1, 2, 3, 4, 5, 6]
+     *
+     * @return Collection
+     */
+	public function merge( array $addition ) {
         $this->array = array_merge( $this->array, $addition );
-		return new static( $this->array );
+        return new static( $this->array );
 	}
 
+    /**
+     * Remove and return the last element from the collection.
+     *
+     * @example
+     * $collection = new Collection([1, 2, 3]);
+     * $result = $collection->pop();
+     * // Returns: Collection object with elements [1, 2]
+     *
+     * @return Collection
+     */
 	public function pop() {
 		return new static( array_pop( $this->array ) );
 	}
 
+    /**
+     * Count the number of elements in the collection.
+     *
+     * @example
+     * $collection = new Collection(['a', 'b', 'c']);
+     * $result = $collection->count();
+     * // Returns: 3
+     *
+     * @return int
+     */
 	public function count() {
 		return count( $this->array );
 	}
 
+    /**
+     * Apply a callback to each element of the collection.
+     *
+     * @param callable $callback
+     * @example
+     * $collection = new Collection([1, 2, 3]);
+     * $result = $collection->map(function($item) { return $item * 2; });
+     * // Returns: Collection object with elements [2, 4, 6]
+     *
+     * @return Collection
+     */
 	public function map( $callback ) {
         if( Collection_Helper::is_assoc( $this->array ) ){
             $keys = array_keys( $this->array );
@@ -55,15 +117,19 @@ class Collection implements \Countable
             return new static( array_map( $callback, $keys, $values ) );
         }
 
-		return  new static( array_map( $callback, $this->array ) );
+        return  new static( array_map( $callback, $this->array ) );
 	}
 
     /**
-     * Sort flat and assoc arrays
+     * Sort the collection either in ascending order.
      *
-     * @param $callback
+     * @param callable|null $callback
+     * @example
+     * $collection = new Collection([3, 1, 2]);
+     * $result = $collection->sort();
+     * // Returns: Collection object with elements [1, 2, 3]
      *
-     * @return $this
+     * @return Collection
      */
     public function sort( $callback = null ) {
         // TODO: test in the future
@@ -81,6 +147,7 @@ class Collection implements \Countable
     }
 
 	/**
+     * TODO: not fully working yet
 	 * @param string $direction ("asc", "desc" )
 	 * @param string $s3ort_by ( "key", "value" )
 	 * @return array
@@ -94,10 +161,16 @@ class Collection implements \Countable
 	}
 
 
-	public function reduce(  ) {
-		return $this->array;
-	}
-
+    /**
+     * Get all elements of the collection.
+     *
+     * @example
+     * $collection = new Collection([1, 2, 3]);
+     * $result = $collection->all();
+     * // Returns: [1, 2, 3]
+     *
+     * @return array
+     */
 	public function all() {
 		return $this->array;
 	}
